1561f6baae84fc4ae109fca9f6ea00edd08a7845
Lekadas <bolognacorsoleandro@gmail.com>
2025-04-11 16:34:15 -0300

    Commit inicial

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/contatos")
+def contatos():
+    return render_template("contatos.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000, host="192.168.0.107") 
